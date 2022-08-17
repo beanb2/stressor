4d7699eb9496d7d7708c45ad638b20999c2919c3
Sam Haycock <sam.haycock@usu.edu>
2022-08-17 17:11:25 -0600

    implemented the three methods will abstract them once they are verified

--- R/scratch/notes.R
+++ R/scratch/notes.R
@@ -2,7 +2,7 @@
 # CV-K means
 # Documentation of other pieces
 
-# Fix the Fitting
+# Fix the Fitting (done)
 # Standard CV
 # K-means
 # Data Thinning by 80/20 and digress (Random Sampling)
@@ -16,25 +16,65 @@ library(mlbench)
 data("BostonHousing2")
 boston <- select(.data = BostonHousing2, -town, -tract, -lon, -lat, -medv, -chas)
 mlm_original <- compare_mlm(cmedv ~ ., data = boston)
+
+accuracy <- function(refitted){
+  pred_accuracy_vec <- rep(0, length = length(refitted))
+  for (j in seq_len(length(refitted))){
+    pred_accuracy_vec[j] <- sqrt(sum((refitted[[j]]$cmedv -
+                                        refitted[[j]]$Label)^2) /
+                                   nrow(refitted[[j]]))
+  }
+  pred_accuracy_vec
+}
+
+
+# Standard CV
+n_folds <- 10
+xvs <- rep(1:n_folds,length = nrow(boston))
+xvs <- sample(xvs)
+pred_accuracy_cv <- matrix(0, nrow = length(mlm_original$models),
+                           ncol = n_folds)
+for(i in 1:10){
+  train <- boston[xvs!=i, ]
+  test <- boston[xvs==i, ]
+  refitted <- refit_mlm(mlm_original, train, test)
+  pred_accuracy_cv[, i] <- accuracy(refitted)
+}
+row.names(pred_accuracy_cv) <- row.names(mlm_original$pred_accuracy)
+colnames(pred_accuracy_cv) <- paste0(seq(1, 10, 1), rep("fold", 10))
+
+# K-means
 boston_feat <- scale(select(.data = boston, -cmedv))
 k_groups <- cluster_part2(boston_feat, 10)
-pred_accuracy1 <- matrix(0, nrow = length(mlm_original$models),
-                        ncol = max(k_groups))
+pred_accuracy_kmeans <- matrix(0, nrow = length(mlm_original$models),
+                               ncol = max(k_groups))
+
 for (i in seq_len(max(k_groups))) {
   test_index <- which(i == k_groups)
   test <- boston[test_index, ]
   train <- boston[-test_index, ]
   refitted <- refit_mlm(mlm_original, train, test)
-  for (j in seq_len(length(refitted))){
-    pred_accuracy1[j, i] <- sqrt(sum((refitted[[j]]$cmedv -
-                                       refitted[[j]]$Label)^2) / nrow(test))
-  }
+  pred_accuracy_kmeans[, i] <- accuracy(refitted)
 }
-row.names(pred_accuracy1) <- row.names(mlm_original$pred_accuracy)
-colnames(pred_accuracy1) <- seq(1, 10, 1)
-pred_accuracy1
+row.names(pred_accuracy_kmeans) <- row.names(mlm_original$pred_accuracy)
+colnames(pred_accuracy_kmeans) <- seq(1, 10, 1)
+pred_accuracy_kmeans
 
-# perfect predictions because it has all of the data
+# Data Thinning 80/20 - 20/80 Train/Test
+train_size <- seq(.8, .2, -.05)
+pred_accuracy_thinning <- matrix(0, nrow = length(mlm_original$models),
+                                 ncol = length(train_size))
+
+for (i in seq_len(length(train_size))) {
+  train_index <- sample(1:nrow(boston), train_size[i] * nrow(boston))
+  test <- boston[-train_index, ]
+  train <- boston[train_index, ]
+  refitted <- refit_mlm(mlm_original, train, test)
+  pred_accuracy_thinning[, i] <- accuracy(refitted)
+}
+row.names(pred_accuracy_thinning) <- row.names(mlm_original$pred_accuracy)
+colnames(pred_accuracy_thinning) <- train_size
+pred_accuracy_thinning
 
 data("Sonar")
 Class <- select(.data = Sonar, Class)

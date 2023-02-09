06d0e44b9139c8ef8750a47bdf97c00adb51948e
Sam Haycock <sam.haycock@usu.edu>
2023-02-09 09:04:49 -0700

    validation

--- scripts/scv_cv.R
+++ scripts/scv_cv.R
@@ -1,18 +1,21 @@
 # Script to run 50 evaluations of CV and SCV
-
+library(foreach)
+library(doParallel)
+total_cores <- detectCores()
+cluster <- makeCluster(total_cores[1] / 2)
+registerDoParallel(cluster)
 validation_rep <- function(formula, data, n_folds = 10, k_mult = NULL,
                            repl = FALSE, grouping_formula = NULL, times = 10) {
-  rmse_mat <- matrix(0, nrow = 18, ncol = times)
+  rmse_list <- vector(mode = "list", length = times)
   data <- model.frame(formula = formula, data = data)
   vv <- attr(terms(formula(data)), which = "variables")
   rr <- as.character(vv[[2]]) # The response variable name
-  for (i in seq_len(times)) {
+  for (i in  1:times) {
     mlm_object <- mlm_regressor(formula, data, example = TRUE)
     cv_method <- cv(mlm_object, data, n_folds, k_mult, repl, grouping_formula)
-    rmse_mat[, i] <- rmse(cv_method[, i], data[, rr])
+    rmse_list[[i]] <- rmse(cv_method, data[, rr])
   }
-  rmse_vec <- apply(rmse_mat, 2, mean)
-  rmse_vec
+  rmse_list
 }
 
 library(tidyverse)
@@ -37,10 +40,9 @@ formula <- YIELD ~
   S_PH_H2O + T_CEC_SOIL + T_REF_BULK_DENSITY + T_OC
 
 validation <- matrix(0, nrow =18, ncol = 3)
-validation[, 1] <- validation_rep(formula, corn_yield, times = 50)
-validation[, 2] <- validation_rep(formula, corn_yield, n_folds = 10, k_mult = 5,
-                                  times = 50)
-validation[, 3] <- validation_rep(formula, corn_yield, n_folds = 10, k_mult = 5,
-                                  grouping_formula = ~ lat + lon, trials,
-                                  times = 50)
+blue <- validation_rep(formula, corn_yield, times = 5)
+red <- validation_rep(formula, corn_yield, n_folds = 10, k_mult = 5,
+                                  times = 5)
+green <- validation_rep(formula, corn_yield, n_folds = 10, k_mult = 5,
+                        grouping_formula = ~ lat + lon, times = 5)
 
